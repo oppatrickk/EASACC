8908fc254e02926defc4334a04fb407874716db2
John Patrick Prieto <prieto.patrick15@gmail.com>
2025-11-20 23:29:58 +0800

    feat(app): organize files

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:easacc/features/webview/webview_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EASACC Task Demo App',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
