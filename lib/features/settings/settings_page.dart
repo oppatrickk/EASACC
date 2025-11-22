3ad7ceaa9d07e3bc0f56b80967af1b3aafe2a2fa
John Patrick Prieto <prieto.patrick15@gmail.com>
2025-11-22 08:09:43 +0800

    feat(settings): adjust overflow

--- lib/features/settings/settings_page.dart
+++ lib/features/settings/settings_page.dart
@@ -45,128 +45,143 @@ class _SettingsPageState extends State<SettingsPage> {
         leadingWidth: 180,
         backgroundColor: context.colorScheme.brand,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              context.l10n.settings_general,
-              style: context.textTheme.bodySmall?.semibold.copyWith(color: context.colorScheme.text),
-            ),
-            SettingsItem(
-              onTap: () {
-                AppWidget.setLocale(
-                  context,
-                  isArabic ? const Locale('en') : const Locale('ar'),
-                );
-              },
-              widget: Row(
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.all(24),
                 children: <Widget>[
                   Text(
-                    isArabic ? 'Arabic' : 'English',
-                    style: context.textTheme.labelMedium?.copyWith(
-                      color: context.colorScheme.onSurfaceVariant,
+                    context.l10n.settings_general,
+                    style: context.textTheme.bodySmall?.semibold.copyWith(color: context.colorScheme.text),
+                  ),
+                  SettingsItem(
+                    onTap: () {
+                      AppWidget.setLocale(
+                        context,
+                        isArabic ? const Locale('en') : const Locale('ar'),
+                      );
+                    },
+                    widget: Row(
+                      children: <Widget>[
+                        Text(
+                          isArabic ? 'Arabic' : 'English',
+                          style: context.textTheme.labelMedium?.copyWith(
+                            color: context.colorScheme.onSurfaceVariant,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                    title: context.l10n.settings_language,
+                    icon: CustomIconData.language,
+                  ),
+
+                  Consumer<ThemeService>(
+                    builder: (BuildContext context, ThemeService themeService, _) {
+                      bool isDarkMode;
+                      if (themeService.themeMode == ThemeMode.system) {
+                        final Brightness brightness = MediaQuery.of(context).platformBrightness;
+                        isDarkMode = brightness == Brightness.dark;
+                      } else {
+                        isDarkMode = themeService.themeMode == ThemeMode.dark;
+                      }
+
+                      return SettingsItem(
+                        onTap: () {
+                          if (themeService.themeMode == ThemeMode.dark || (themeService.themeMode == ThemeMode.system && isDarkMode)) {
+                            themeService.setThemeMode(ThemeMode.light);
+                          } else {
+                            themeService.setThemeMode(ThemeMode.dark);
+                          }
+                        },
+                        widget: Row(
+                          children: <Widget>[
+                            Text(
+                              isDarkMode ? context.l10n.settings_dark : context.l10n.settings_light,
+                              style: context.textTheme.labelMedium?.copyWith(color: context.colorScheme.onSurfaceVariant),
+                            ),
+                            const SizedBox(width: 8),
+                          ],
+                        ),
+                        title: context.l10n.settings_theme_mode,
+                        icon: CustomIconData.theme,
+                      );
+                    },
+                  ),
+
+                  const SizedBox(height: 32),
+
+                  Text(
+                    context.l10n.settings_webview,
+                    style: context.textTheme.bodySmall?.semibold.copyWith(color: context.colorScheme.text),
+                  ),
+
+                  SettingsItem(
+                    onTap: () async {
+                      final String? result = await showDialog(
+                        context: context,
+                        builder: (_) => WebViewInput(),
+                      );
+                      if (result == null) return;
+
+                      await SharedPreferenceService.saveWebViewUrl(result);
+                      setState(() {});
+                    },
+                    title: context.l10n.settings_webview_configure,
+                    icon: CustomIconData.url,
+                  ),
+
+                  const SizedBox(height: 32),
+
+                  Text(
+                    context.l10n.settings_network_devices,
+                    style: context.textTheme.bodySmall?.semibold.copyWith(color: context.colorScheme.text),
+                  ),
+
+                  SettingsItem(
+                    title: context.l10n.settings_configure,
+                    icon: CustomIconData.devices,
+                  ),
+
+                  const SizedBox(height: 40),
+                  Center(
+                    child: Text(
+                      '© John Patrick Prieto',
+                      style: context.textTheme.bodySmall?.copyWith(color: context.colorScheme.text),
                     ),
                   ),
-                  const SizedBox(width: 8),
-                ],
-              ),
-              title: context.l10n.settings_language,
-              icon: CustomIconData.language,
-            ),
-            Consumer<ThemeService>(
-              builder: (BuildContext context, ThemeService themeService, Widget? child) {
-                bool isDarkMode;
-                if (themeService.themeMode == ThemeMode.system) {
-                  final Brightness brightness = MediaQuery.of(context).platformBrightness;
-                  isDarkMode = brightness == Brightness.dark;
-                } else {
-                  isDarkMode = themeService.themeMode == ThemeMode.dark;
-                }
-
-                return SettingsItem(
-                  onTap: () {
-                    if (themeService.themeMode == ThemeMode.dark || (themeService.themeMode == ThemeMode.system && isDarkMode)) {
-                      themeService.setThemeMode(ThemeMode.light);
-                    } else {
-                      themeService.setThemeMode(ThemeMode.dark);
-                    }
-                  },
-                  widget: Row(
-                    children: <Widget>[
-                      Text(
-                        isDarkMode ? context.l10n.settings_dark : context.l10n.settings_light,
-                        style: context.textTheme.labelMedium?.copyWith(color: context.colorScheme.onSurfaceVariant),
-                      ),
-                      SizedBox(width: 8),
-                    ],
-                  ),
-                  title: context.l10n.settings_theme_mode,
-                  icon: CustomIconData.theme,
-                );
-              },
-            ),
-            const SizedBox(height: 32),
-            Text(
-              context.l10n.settings_webview,
-              style: context.textTheme.bodySmall?.semibold.copyWith(color: context.colorScheme.text),
-            ),
-            SettingsItem(
-              onTap: () async {
-                final String? result = await showDialog(
-                  context: context,
-                  builder: (_) => WebViewInput(),
-                );
-                if (result == null) return;
-
-                await SharedPreferenceService.saveWebViewUrl(result);
-                setState(() {});
-              },
-              title: context.l10n.settings_webview_configure,
-              icon: CustomIconData.url,
-            ),
-            const SizedBox(height: 32),
-            Text(
-              context.l10n.settings_network_devices,
-              style: context.textTheme.bodySmall?.semibold.copyWith(color: context.colorScheme.text),
-            ),
-            SettingsItem(
-              title: context.l10n.settings_configure,
-              icon: CustomIconData.devices,
-            ),
-            Spacer(),
-            Center(
-              child: Text(
-                '© John Patrick Prieto',
-                style: context.textTheme.bodySmall?.copyWith(color: context.colorScheme.text),
-              ),
-            ),
-            const SizedBox(height: 16),
-            OutlinedButton(
-              onPressed: () {
-                Navigator.pop(context);
-                context.pushReplacement(const LoginPage());
-              },
-              style: ButtonStyle(
-                padding: WidgetStateProperty.all<EdgeInsets>(EdgeInsets.symmetric(horizontal: 16, vertical: 16)),
-                side: WidgetStateProperty.all<BorderSide>(BorderSide(color: context.colorScheme.error, width: 1.5)),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    context.l10n.settings_logout,
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis,
-                    style: context.textTheme.titleMedium?.copyWith(color: context.colorScheme.error),
-                  ),
                 ],
               ),
             ),
-            SizedBox(height: 16),
+
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  context.pushReplacement(const LoginPage());
+                },
+                style: ButtonStyle(
+                  padding: WidgetStateProperty.all(
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  ),
+                  side: WidgetStateProperty.all(
+                    BorderSide(color: context.colorScheme.error, width: 1.5),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      context.l10n.settings_logout,
+                      style: context.textTheme.titleMedium?.copyWith(color: context.colorScheme.error),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
